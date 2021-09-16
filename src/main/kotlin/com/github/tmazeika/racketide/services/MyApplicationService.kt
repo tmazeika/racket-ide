c6dafc8533beb61818722266ad6166855d1a29b4
GitHub Action <action@github.com>
2021-09-16 00:09:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/tmazeika/racketide/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tmazeika/racketide/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tmazeika.racketide.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tmazeika.racketide.MyBundle
 
 class MyApplicationService {
 
